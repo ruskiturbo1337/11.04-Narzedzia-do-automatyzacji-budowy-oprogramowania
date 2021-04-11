498974a280ec91b7e3ac4d154be76659b2ada6c1
szymonbielecki <bieleckis@o2.pl>
2021-04-11 12:09:53 +0200

    ez`

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = 2
-y = 5
+x = 3
+y = 4
 z = x + y
 print("Hello World")
 print("Witaj świecie")
 print(z)
+
+a = int(input("podaj x"))
+b = int(input("podaj y"))
+c = 2*a + 2*b
+print("obwód prostokata wynosi", c)
