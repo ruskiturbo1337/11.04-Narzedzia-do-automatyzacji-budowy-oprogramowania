f906097919cf7ab490113d6d93283c7b1fd7ca6c
BartoszParuch <bartosz.paruch99@gmail.com>
2021-04-25 12:47:43 +0200

    Kalkulator 1.1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def menu():
     print("3. Mnożenie")
     print("4. Dzielenie")
     print("5. Potęgowanie")
+    print("6. Pierwiastkowanie")
 
 def dodaj(x,y):
     return x + y
@@ -20,22 +21,52 @@ def pomnoz(x,y):
 def podziel(x,y):
     return x / y
 
+def poteguj(x,y):
+    return x ** y
+
 menu()
 
 wybor = input(" Wybierz funkcję : ")
 
 if wybor == '1':
-    print(x,"+",y,"=", dodaj(x,y))
+    while True:
+        try:
+            x = float(input("Podaj liczbę x: "))
+            y = float(input("Podaj liczbę y: "))
+            x + y
+            break
+        except ValueError:
+            print('Podaj poprawne wartości!')
+            continue
+    print(x, "+", y, "=", dodaj(x, y))
 elif wybor == '2':
+    while True:
+        try:
+            x = float(input("Podaj liczbę x: "))
+            y = float(input("Podaj liczbę y: "))
+            x - y
+            break
+        except ValueError:
+            print('Podaj poprawne wartości!')
+            continue
     print(x,"-",y,"=", odejmij(x,y))
 elif wybor == '3':
-    print(x,"*",y,"=", pomnoz(x,y))
+    while True:
+        try:
+            x = float(input("Podaj liczbę x: "))
+            y = float(input("Podaj liczbę y: "))
+            x * y
+            break
+        except ValueError:
+            print('Podaj poprawne wartości!')
+            continue
+    print(x, "*", y, "=", pomnoz(x, y))
 elif wybor == '4':
     while True:
         try:
-            a = float(input("Podaj liczbę a którą chcesz podzielić: "))
-            b = float(input("Podaj dzielnik: "))
-            a / b
+            x = float(input("Podaj liczbę a którą chcesz podzielić: "))
+            y = float(input("Podaj dzielnik: "))
+            x / y
             break
         except ValueError:
             print('Podaj poprawne wartości!')
@@ -44,4 +75,17 @@ elif wybor == '4':
         except ZeroDivisionError:
             print('Nigdy cholero nie dziel przez zero!')
             continue
-    print(a, "/", b, "=", podziel(a, b))        
+    print(x, "/", y, "=", podziel(x, y))
+elif wybor == '5':
+    while True:
+        try:
+            x = float(input("Podaj podstawę potęgi: "))
+            y = float(input("Podaj wykładnik: "))
+            x ** y
+            break
+        except ValueError:
+            print('Podaj poprawne wartości!')
+            continue
+    print(x, "**", y, "=", poteguj(x, y))
+else:
+    print("Wybierz prawidłową opcję!")
