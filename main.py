562ee8acd666607dad464b1ea4c4d811fdabdf6c
BartoszParuch <bartosz.paruch99@gmail.com>
2021-04-25 11:26:33 +0200

    odejmowanie :)

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ def menu():
     print("5. Potęgowanie")
 
 def dodaj(x,y):
-    wynik=x+y
-    return wynik
+    return x + y
+
+def odejmij(x,y):
+    return x - y
+
 print(menu())
 wybor = input(" Wybierz funkcję : ");
 x=input("Podaj x: ")
 y=input("Podaj y: ")
+
 if wybor == 1:
-    print(dodaj(x,y))
+    print(x,"+",y, dodaj(x,y))
+elif choice == 2:
+    print (x,"-",y, odejmij(x,y))
